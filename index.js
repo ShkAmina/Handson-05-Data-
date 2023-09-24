64e5d85f4687dedc6bf45d481d010defc7c97fc2
sara <shksara2000@gmail.com>
2023-09-24 16:53:41 +0530

    Handson-05

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const http = require('http').Server(app)
 const port = 3002;
-const cors = require("cors")
+// const cors = require("cors")
 // const { Server } = require("socket.io")
 // app.use(cors())
 
