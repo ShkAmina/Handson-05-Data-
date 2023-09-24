4964d34c22dfd0927100d816f120788330a2c226
sara <shksara2000@gmail.com>
2023-09-24 16:02:06 +0530

    Handson-05

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express();
+const http = require('http').Server(app)
+const port = 3002;
+// const cors = require("cors")
+// const { Server } = require("socket.io")
+// app.use(cors())
+
+// const server = http.createServer(app)
+
+const io = require('socket.io')(http,{
+    cors: {
+        origin: "*"
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running in Port...${port}`)
+});
+
+io.on('connection', (socket) => {
+    console.log(`User Connected`);
+
+    socket.on("join", (userName) => {
+        socket.userName=userName;
+    })
+    socket.on("message", (data) => {
+
+        console.log("Message Recived",data);
+        io.emit("message",{
+            userName : socket.userName,
+            message: data.message,
+        })
+    })
+})
